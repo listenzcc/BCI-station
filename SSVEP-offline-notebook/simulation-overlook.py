d7bf63ff107cac83db52ba0f8fa3923a5f8f0ffc
listenzcc <cc.public@outlook.com>
2025-01-09 12:28:43 +0800

    Remove inline package installation from simulation-overlook.py and add requirements.txt for dependency management

--- SSVEP-offline-notebook/simulation-overlook.py
+++ SSVEP-offline-notebook/simulation-overlook.py
@@ -18,23 +18,12 @@ Functions:
 
 # %% ---- 2025-01-09 ------------------------
 # Requirements and constants
-import subprocess
-
-required_packages = ['matplotlib', 'seaborn', 'scipy']
-
-for package in required_packages:
-    try:
-        __import__(package)
-    except ImportError:
-        subprocess.check_call(["python", '-m', 'pip', 'install', package])
-
 
 import seaborn as sns
 import streamlit as st
 from scipy.stats import multivariate_normal
 import matplotlib.pyplot as plt
 import numpy as np
-import os
 
 
 # Set seaborn style
